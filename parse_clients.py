72d36f03e22a6d000160bfc969aa4582fd84550f
Mitchell Moore <mmoo97@uab.edu>
2024-12-10 09:58:18 -0600

    Conditionally switch screens via button input.

--- parse_clients.py
+++ parse_clients.py
@@ -95,31 +95,56 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    Clients = "Clients: " + str(len(clients))
-    # Write four lines of text.
-    y = top
-    draw.text((x, y), Clients, font=font, fill="#FFFFFF")
-    y += font.getsize(Clients)[1]
-    # draw.text((x, y), CPU, font=font, fill="#FFFF00")
-    # y += font.getsize(CPU)[1]
-    # draw.text((x, y), MemUsage, font=font, fill="#00FF00")
-    # y += font.getsize(MemUsage)[1]
-    # draw.text((x, y), Disk, font=font, fill="#0000FF")
-    # y += font.getsize(Disk)[1]
-    # draw.text((x, y), Temp, font=font, fill="#FF00FF")
 
     
 
     if buttonA.value and buttonB.value:
-        backlight.value = False  # turn off backlight
+        #backlight.value = False  # turn off backlight
+        Clients = "Clients: " + str(len(clients))
+        # Write four lines of text.
+        y = top
+        draw.text((x, y), Clients, font=font, fill="#FFFFFF")
+        y += font.getsize(Clients)[1]
+        # draw.text((x, y), CPU, font=font, fill="#FFFF00")
+        # y += font.getsize(CPU)[1]
+        # draw.text((x, y), MemUsage, font=font, fill="#00FF00")
+        # y += font.getsize(MemUsage)[1]
+        # draw.text((x, y), Disk, font=font, fill="#0000FF")
+        # y += font.getsize(Disk)[1]
+        # draw.text((x, y), Temp, font=font, fill="#FF00FF")
     else:
-        backlight.value = True  # turn on backlight
+        #backlight.value = True  # turn on backlight
+        
     # if buttonB.value and not buttonA.value:  # just button A pressed
     #     display.fill(color565(255, 0, 0))  # red
     # if buttonA.value and not buttonB.value:  # just button B pressed
     #     display.fill(color565(0, 0, 255))  # blue
-    # if not buttonA.value and not buttonB.value:  # none pressed
-    #     display.fill(color565(0, 255, 0))  # green
+    if not buttonA.value and not buttonB.value:  # none pressed
+        #display.fill(color565(0, 255, 0))  # green
+        # Shell scripts for system monitoring from here:
+        # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+        cmd = "hostname -I | cut -d' ' -f1"
+        IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
+        cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+        CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+        cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+        Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+        cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
+        Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+        # Write four lines of text.
+        y = top
+        draw.text((x, y), IP, font=font, fill="#FFFFFF")
+        y += font.getsize(IP)[1]
+        draw.text((x, y), CPU, font=font, fill="#FFFF00")
+        y += font.getsize(CPU)[1]
+        draw.text((x, y), MemUsage, font=font, fill="#00FF00")
+        y += font.getsize(MemUsage)[1]
+        draw.text((x, y), Disk, font=font, fill="#0000FF")
+        y += font.getsize(Disk)[1]
+        draw.text((x, y), Temp, font=font, fill="#FF00FF")
         
     # Display image.
     disp.image(image, rotation)
